7dabd7473ac86469bfbe87154e481537f7485bea
oppih-374 <letwego28@gmail.com>
2012-01-28 00:01:16 +0800

    Level 4 finished

---
+++ player.rb
@@ -0,0 +1,14 @@
+class Player
+  def play_turn(warrior)
+    if warrior.feel.empty?
+      if warrior.health == 20 || warrior.health < @health
+        warrior.walk!
+      else
+        warrior.rest!
+      end
+    else
+      warrior.attack!
+    end
+    @health = warrior.health
+  end
+end
