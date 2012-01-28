ffa2157e84afd15cdae96bfee1c08b41db77296a
oppih-374 <letwego28@gmail.com>
2012-01-28 10:15:56 +0800

    Level 6 occured, but not fast enough

--- player.rb
+++ player.rb
@@ -1,13 +1,19 @@
 class Player
   def play_turn(warrior)
     if warrior.feel.empty?
-      if warrior.health == 20 || warrior.health < @health
-        warrior.walk!
-      else
+      if @rescued != true && warrior.feel(:backward).empty?
+        warrior.walk!(:backward)
+      elsif warrior.feel(:backward).captive?
+        warrior.rescue!(:backward)
+        @rescued = true
+      elsif warrior.feel(:backward).empty? && warrior.health < 8
+        #warrior.rest!
+        warrior.walk!(:backward)
+      elsif warrior.feel(:backward).wall? && warrior.health < 15
         warrior.rest!
+      else warrior.feel(:backward).wall?
+        warrior.walk!
       end
-    elsif warrior.feel.captive?
-      warrior.rescue!
     else
       warrior.attack!
     end
